2dadb4a57cdd2fd44df8449fb54b8cbae46c019c
Pete <Pete>
2024-12-25 08:46:09 +0800

    feat: update baseURL

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
+    baseURL: `/api`,
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json'
