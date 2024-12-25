5d6b8ae620e3252b7c66605c6ac7af88392f32fa
Pete <Pete>
2024-12-25 08:40:39 +0800

    feat: update NEXT_PUBLIC_API_URL

--- src/lib/http-client.ts
+++ src/lib/http-client.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
     timeout: 30000,
     headers: {
         'Content-Type': 'application/json'
