a815136598b149e44f105f5a248f8950703bbacd
Pete <Pete>
2024-12-27 15:34:06 +0800

    feat: update node-fetch

--- src/services/website-parser/impls/fetch-xml-service.ts
+++ src/services/website-parser/impls/fetch-xml-service.ts
@@ -1,8 +1,9 @@
 import { FetchXmlService } from "../fetch-xml-service"
-import fetch from 'node-fetch'
+// import fetch from 'node-fetch'
 
 export class FetchXmlServiceImpl implements FetchXmlService {
-    fetchXml(url: string): Promise<string> {
+    async fetchXml(url: string): Promise<string> {
+        const fetch = (await import('node-fetch')).default;
         return fetch(url).then(res => res.text())
     }
 }
