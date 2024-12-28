4cffbdaabb001ff925200bd11af6d27524d537b5
Pete <Pete>
2024-12-28 21:59:44 +0800

    feat: node-fetch

--- src/services/website-parser/impls/fetch-html-service.ts
+++ src/services/website-parser/impls/fetch-html-service.ts
@@ -1,11 +1,10 @@
 import { injectable } from "inversify";
 import { FetchHtmlService } from "../fetch-html-service";
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 
 @injectable()
 export class FetchHtmlServiceImpl implements FetchHtmlService {
     async fetchHtml(url: string): Promise<string> {
-        const fetch = (await import('node-fetch')).default;
         return fetch(url).then(res => res.text())
     }
 }
--- src/services/website-parser/impls/fetch-xml-service.ts
+++ src/services/website-parser/impls/fetch-xml-service.ts
@@ -1,9 +1,8 @@
 import { FetchXmlService } from "../fetch-xml-service"
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 
 export class FetchXmlServiceImpl implements FetchXmlService {
     async fetchXml(url: string): Promise<string> {
-        const fetch = (await import('node-fetch')).default;
         return fetch(url).then(res => res.text())
     }
 }
